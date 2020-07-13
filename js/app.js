0123d5db2cf103886caf6c6a50296e021bc88909
mahesh <maheshsg297@gmail.com>
2020-07-13 19:48:06 +0530

    Code format change

--- js/app.js
+++ js/app.js
@@ -49,11 +49,25 @@ const createOption = (text) => {
 //   }
 // );
 
+//! async await
+// const getData = async () => {
+//   const res = await fetch(apiUrl);
+//   const data = await res.json();
+//   return data;
+// };
+
+// getData()
+//   .then((data) => {
+//     console.log(data.services[0]);
+//   })
+//   .catch(console.log("Error caught"));
+
+//! promise
 const getData = () => {
   return new Promise((resolve, reject) => {
-    fetch(apiUrl).then((result) => {
-      if (result.status === 200) {
-        result.json().then((data) => {
+    fetch(apiUrl).then((response) => {
+      if (response.status === 200) {
+        response.json().then((data) => {
           resolve(data);
         });
       } else {
@@ -70,9 +84,9 @@ getData().then(
       optionsWrapper.appendChild(createdOption);
     });
 
-    let script = document.createElement("script");
-    script.src = "./js/dropDownStyle.js";
-    document.body.appendChild(script);
+    import("./dropDownStyle.js").then((module) => {
+      module.default();
+    });
   },
   (err) => console.log(err)
 );
--- js/dropDownStyle.js
+++ js/dropDownStyle.js
@@ -1,130 +1,141 @@
 //! dropdown menu styling
+
+const selectMenu = document.querySelector(
+  ".service-wrapper .menu-wrapper .select"
+);
+const optionsWrapper = document.querySelector(
+  ".service-wrapper .menu-wrapper .options"
+);
 const options = document.querySelectorAll(
   ".service-wrapper .menu-wrapper .options div"
 );
 
-const displayOptions = () => {
-  optionsWrapper.classList.remove("hide");
-  optionsWrapper.classList.add("show");
-};
-const hideOptions = () => {
-  optionsWrapper.classList.remove("show");
-  optionsWrapper.classList.add("hide");
-};
+//! class
+class DropDownStyle {
+  constructor() {
+    this.lastKnownOption;
+  }
 
-const toggleOptions = () => {
-  if (optionsWrapper.classList.contains("hide")) {
-    displayOptions();
-    focusLastKnownoption();
-    reflectOPtionchange();
-  } else if (optionsWrapper.classList.contains("show")) {
-    hideOptions();
-  } else {
+  displayOptions() {
+    optionsWrapper.classList.remove("hide");
     optionsWrapper.classList.add("show");
-    focusLastKnownoption();
-    reflectOPtionchange();
   }
-};
-
-let lastKnownoption;
-const focusLastKnownoption = () => {
-  if (typeof lastKnownoption === "undefined") {
-    options[0].focus();
-  } else {
-    i = getOptionIndex(lastKnownoption);
-    options[i].focus();
-  }
-};
-
-const getOptionIndex = (currentEle) => {
-  if (typeof currentEle === "undefined") {
-    currentEle = document.activeElement;
+  hideOptions() {
+    optionsWrapper.classList.remove("show");
+    optionsWrapper.classList.add("hide");
   }
 
-  for (let i = 0; i < options.length; i++) {
-    if (currentEle.className === options[i].className) return i;
-  }
-};
-
-const reflectOPtionchange = (i) => {
-  if (typeof i === "undefined") {
-    i = getOptionIndex();
-  }
-  selectMenu.children[0].innerText = options[i].innerText;
-};
-
-const changeOption = (selectedOptionEle, selectedOptionText) => {
-  lastKnownoption = selectedOptionEle;
-  selectMenu.children[0].innerText = selectedOptionText;
-  selectMenu.focus();
-  hideOptions();
-};
-
-selectMenu.addEventListener("click", () => {
-  toggleOptions();
-});
-
-let i = -1;
-selectMenu.addEventListener("keydown", (e) => {
-  if (e.code === "Space" || e.code === "Enter") {
-    e.preventDefault();
-    toggleOptions();
-  } else if (e.code === "ArrowDown") {
-    e.preventDefault();
-    i++;
-    if (i < options.length) {
-      selectMenu.children[0].innerText = options[i].innerText;
+  toggleOptions() {
+    if (optionsWrapper.classList.contains("hide")) {
+      this.displayOptions();
+      this.focusLastKnownoption();
+      this.reflectOPtionchange();
+    } else if (optionsWrapper.classList.contains("show")) {
+      this.hideOptions();
     } else {
-      i--;
+      optionsWrapper.classList.add("show");
+      this.focusLastKnownoption();
+      this.reflectOPtionchange();
     }
-  } else if (e.code === "ArrowUp") {
-    e.preventDefault();
-    i--;
-    if (i >= 0) {
-      selectMenu.children[0].innerText = options[i].innerText;
+  }
+
+  focusLastKnownoption() {
+    if (typeof this.lastKnownOption === "undefined") {
+      options[0].focus();
     } else {
+      let i = this.getOptionIndex(this.lastKnownOption);
+      options[i].focus();
+    }
+  }
+
+  getOptionIndex(currentEle) {
+    if (typeof currentEle === "undefined") {
+      currentEle = document.activeElement;
+    }
+
+    for (let i = 0; i < options.length; i++) {
+      if (currentEle.className === options[i].className) return i;
+    }
+  }
+
+  reflectOPtionchange(i) {
+    if (typeof i === "undefined") {
+      i = this.getOptionIndex();
+    }
+    selectMenu.children[0].innerText = options[i].innerText;
+  }
+
+  changeOption() {
+    this.lastKnownOption = document.activeElement;
+    selectMenu.children[0].innerText = document.activeElement.textContent;
+    selectMenu.focus();
+    this.hideOptions();
+  }
+}
+
+//! main
+export default function () {
+  let dropDown = new DropDownStyle();
+
+  selectMenu.addEventListener("click", () => {
+    dropDown.toggleOptions();
+  });
+
+  let i = -1;
+  selectMenu.addEventListener("keydown", (e) => {
+    if (e.code === "Space" || e.code === "Enter") {
+      e.preventDefault();
+      dropDown.toggleOptions();
+    } else if (e.code === "ArrowDown") {
+      e.preventDefault();
       i++;
+      if (i < options.length) {
+        selectMenu.children[0].innerText = options[i].innerText;
+      } else {
+        i--;
+      }
+    } else if (e.code === "ArrowUp") {
+      e.preventDefault();
+      i--;
+      if (i >= 0) {
+        selectMenu.children[0].innerText = options[i].innerText;
+      } else {
+        i++;
+      }
     }
-  }
 
-  lastKnownoption = options[i];
-});
+    dropDown.lastKnownOption = options[i];
+  });
 
-optionsWrapper.addEventListener("keydown", (e) => {
-  e.preventDefault();
+  optionsWrapper.addEventListener("keydown", (e) => {
+    e.preventDefault();
 
-  let i = getOptionIndex();
+    let i = dropDown.getOptionIndex();
 
-  if (e.code === "ArrowDown") {
-    i++;
-    if (i < options.length) {
-      options[i].focus();
-      reflectOPtionchange(i);
+    if (e.code === "ArrowDown") {
+      i++;
+      if (i < options.length) {
+        options[i].focus();
+        dropDown.reflectOPtionchange(i);
+      }
+    } else if (e.code === "ArrowUp") {
+      i--;
+      if (i >= 0) {
+        options[i].focus();
+        dropDown.reflectOPtionchange(i);
+      }
+    } else if (e.code === "Enter" || e.code === "Escape") {
+      dropDown.changeOption();
     }
-  } else if (e.code === "ArrowUp") {
-    i--;
-    if (i >= 0) {
-      options[i].focus();
-      reflectOPtionchange(i);
-    }
-  } else if (e.code === "Enter" || e.code === "Escape") {
-    changeOption(document.activeElement, document.activeElement.textContent);
-  }
-});
+  });
 
-optionsWrapper.addEventListener("click", (e) => {
-  e.preventDefault();
-  changeOption(e.target, e.target.textContent);
-});
+  optionsWrapper.addEventListener("click", () => {
+    dropDown.changeOption();
+  });
 
-optionsWrapper.addEventListener("mousemove", (e) => {
-  e.preventDefault();
-  document.activeElement.blur();
-  e.target.focus();
-});
-
-// for (let i = 0; i < options.length; i++) {
-//   if (options[i].className != document.activeElement.className) {
-//     hideOptions();
-//   }
-// }
+  optionsWrapper.addEventListener("mousemove", (e) => {
+    e.preventDefault();
+    document.activeElement.blur();
+    e.target.focus();
+  });
+}
